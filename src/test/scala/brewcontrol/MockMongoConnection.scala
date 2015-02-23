d7ee685479cc90652d68c46a95296b84bd316896
Christoph Dietze <christoph.dietze@gmail.com>
2015-02-23 21:39:01 +0100

    Fail sooner fall softer!

--- src/test/scala/brewcontrol/MockMongoConnection.scala
+++ src/test/scala/brewcontrol/MockMongoConnection.scala
@@ -1,14 +1,21 @@
 package brewcontrol
 
+import com.mongodb.MongoTimeoutException
 import com.mongodb.casbah.Imports._
 
 class MockMongoConnection extends MongoConnection {
 
-  val mongoClient = MongoClient("localhost", 27017)
+  val mongoClient = MongoClient("localhost:27017", MongoClientOptions(connectTimeout = 1000))
 
   override def db = mongoClient("brewcontrol")
 
   def reset() = db.dropDatabase()
+
+  try {
+    mongoClient.getDatabaseNames()
+  } catch {
+    case e: MongoTimeoutException => sys.error("MongoDB not found, did you really start it?")
+  }
 
   reset()
 }
