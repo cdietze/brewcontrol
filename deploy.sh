deebe4c1b49bf1464c460436931171b011313858
Christoph Dietze <christoph.dietze@gmail.com>
2015-02-23 21:39:24 +0100

    Added /etc/init.d/brewcontrol restart to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-set -e
+set -e -x
 sbt warn assembly
 scp target/scala-2.11/brewcontrol-assembly-0.1-SNAPSHOT.jar pi:~/brewcontrol
-# ssh pi sudo java -jar ./brewcontrol/brewcontrol-assembly-0.1-SNAPSHOT.jar
+rsync -avz ./root/ pi:~/brewcontrol/root/
+ssh pi sudo cp -rv /home/pi/brewcontrol/root/* /
+ssh pi sudo /etc/init.d/brewcontrol restart
