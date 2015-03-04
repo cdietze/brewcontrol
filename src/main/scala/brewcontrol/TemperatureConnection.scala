536ef3d54f023b95a9fc402b3cef9d226a4b4137
Christoph Dietze <christoph.dietze@gmail.com>
2015-03-04 21:13:37 +0100

    Make TemperatureConnection more tolerant

--- src/main/scala/brewcontrol/TemperatureConnection.scala
+++ src/main/scala/brewcontrol/TemperatureConnection.scala
@@ -1,18 +1,28 @@
 package brewcontrol
 
+import java.io.File
+
 import scala.io.Source
 import scala.util.Try
 
 /** Low level interface to deal with temperature sensors */
 class TemperatureConnection {
 
+  private object Paths {
+    def w1MasterSlaves: String = "/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves"
+    def w1Slave(sensorId: String): String = s"/sys/bus/w1/devices/$sensorId/w1_slave"
+  }
+
   def sensorIds(): Try[Set[String]] = {
-    val path = "/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves"
-    Try(Source.fromFile(path).getLines().toSet)
+    Try {
+      val ids = Source.fromFile(Paths.w1MasterSlaves).getLines().toSet
+      // Filter out any devices that don't have w1_slave files. Whatever they are, they aren't the sensors we seek
+      ids.filter(id => new File(Paths.w1Slave(id)).exists())
+    }
   }
 
   def temperature(sensorId: String): Try[Float] = {
-    val path = s"/sys/bus/w1/devices/$sensorId/w1_slave"
+    val path = Paths.w1Slave(sensorId)
     Try {
       val lines = Source.fromFile(path).getLines().toList
       val floats = (for {
