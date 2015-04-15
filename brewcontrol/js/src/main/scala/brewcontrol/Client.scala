d66133c11d8965de427b984d75511d85986a34a2
Christoph Dietze <christoph.dietze@gmail.com>
2015-04-15 21:50:35 +0200

    Refactoring: Moved Plot into its own package

--- brewcontrol/js/src/main/scala/brewcontrol/Client.scala
+++ brewcontrol/js/src/main/scala/brewcontrol/Client.scala
@@ -1,23 +1,18 @@
 package brewcontrol
 
 import org.scalajs.dom
-import org.scalajs.dom.ext.Ajax
 import org.scalajs.dom.html
 import org.scalajs.dom.raw.HTMLSelectElement
 import rx._
 import rx.ops._
 
 import scala.collection.mutable
-import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.scalajs.concurrent.JSExecutionContext.Implicits.runNow
 import scala.scalajs.js
 import scala.scalajs.js.Date
-import scala.scalajs.js.Dynamic.literal
 import scala.scalajs.js.annotation.JSExport
 import scalatags.JsDom.all._
-
-
 
 @JSExport
 object Client {
@@ -145,80 +140,3 @@ object Client {
   }
 }
 
-class Plot(plotContainer: dom.Element, messagesContainer: dom.Element) {
-
-  val data = js.Array()
-  val options = literal(
-    "legend" -> literal(
-      "position" -> "nw"
-    ),
-    "series" -> literal("shadowSize" -> 0),
-    "xaxis" -> literal(
-      "mode" -> "time",
-      "timezone" -> "browser"
-    ),
-    "yaxes" -> js.Array(
-      literal("position" -> "right"),
-      literal("show" -> false, "min" -> -0.1, "max" -> 1.1)
-    ))
-  val plot = js.Dynamic.global.jQuery.plot(plotContainer, data, options)
-
-  val o = Client.currentHourRx.foreach { hour =>
-    update(hour)
-  }
-
-  def update(hour: Long): Unit = {
-    messagesContainer.innerHTML = ""
-    val temperatureFutures: Future[List[js.Object]] = ServerApi.temperatures().flatMap(readings => {
-      val futures = readings.map(
-        reading => getTemperatureData(reading.sensorId, reading.name, hour).map(Some(_)).recover { case x => {
-          messagesContainer.appendChild(div(s"No temperature data available for: ${reading.name}").render)
-          None
-        }
-        }
-      )
-      Future.sequence(futures).map(_.flatten)
-    })
-    val relayFutures: Future[List[js.Object]] = ServerApi.relayStates().flatMap(states => {
-      val futures = states.map(
-        state => getRelayData(state.name, hour).map(Some(_)).recover { case x => {
-          messagesContainer.appendChild(div(s"No relay data available for: ${state.name}").render)
-          None
-        }
-        }
-      )
-      Future.sequence(futures).map(_.flatten)
-    })
-
-    val allFutures = Future.sequence(List(temperatureFutures, relayFutures)).map(_.flatten)
-    allFutures.map(seriesList => {
-      val data: js.Array[js.Object] = js.Array()
-      seriesList.foreach(s => data.push(s))
-      updateData(data)
-    })
-  }
-
-  def getTemperatureData(sensorId: String, name: String, hour: Long): Future[js.Object] = {
-    ServerApi.temperatureHourData(sensorId, hour).map(hourData => {
-      literal("label" -> name, "data" -> hourDataToSeries(hourData))
-    })
-  }
-
-  def getRelayData(relayName: String, hour: Long): Future[js.Object] = {
-    ServerApi.relayHourData(relayName, hour).map(hourData => {
-      literal("label" -> relayName, "data" -> hourDataToSeries(hourData), "lines" -> literal("show" -> true, "steps" -> true), "yaxis" -> 2)
-    })
-  }
-
-  def hourDataToSeries(hourData: HourTimeData): js.Array[_] = {
-    val data: js.Array[js.Array[js.Any]] = js.Array()
-    hourData.values.foreach { case (k, v) => data.push(js.Array(k.toLong * 1000L + hourData.hourTimestamp, v)) }
-    data
-  }
-
-  def updateData(data: js.Array[_]): Unit = {
-    plot.setData(data)
-    plot.setupGrid()
-    plot.draw()
-  }
-}
