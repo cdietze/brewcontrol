632d19c0be970f4d79be584923ea065a0e08eaf3
Christoph Dietze <christoph.dietze@gmail.com>
2015-08-17 23:33:44 +0200

    Add some initial mock data for relay states in history

--- jvm/src/test/scala/brewcontrol/MockBrewApp.scala
+++ jvm/src/test/scala/brewcontrol/MockBrewApp.scala
@@ -1,8 +1,18 @@
 package brewcontrol
 
+import brewcontrol.History.Item
+import org.joda.time.DateTime
+
 import scala.util.{Random, Try}
 
 object MockBrewApp extends AbstractBrewApp {
+
+  val startTime = DateTime.now()
+  History.addItem("Heizung", "binary", Item(startTime + 10000, 0))
+  History.addItem("Heizung", "binary", Item(startTime + 12000, 1))
+  History.addItem("Heizung", "binary", Item(startTime + 14000, 0))
+  History.addItem("Heizung", "binary", Item(startTime + 20000, 1))
+
   override def port = 8888
   override def jdbcUrl = "jdbc:sqlite:data.sqlite"
 
