03eda1b2c37d7c2c42113770542826adf5c137bb
Christoph Dietze <christoph.dietze@gmail.com>
2016-02-28 22:29:17 +0100

    Add static recipe stuff

--- ui/src/app/Main.jsx
+++ ui/src/app/Main.jsx
@@ -1,8 +1,3 @@
-/**
- * In this file, we create a React component
- * which incorporates components providedby material-ui.
- */
-
 import React from 'react';
 import RaisedButton from 'material-ui/lib/raised-button';
 import Dialog from 'material-ui/lib/dialog';
@@ -51,54 +46,51 @@ class Main extends React.Component {
     }
 
     render() {
-        const standardActions = (
-            <FlatButton
-                label="Okey"
-                secondary={true}
-                onTouchTap={this.handleRequestClose}
-            />
-        );
+        const relayStyle = {display: 'inline-block', padding: '10px'};
+        const relayStyleOn = Object.assign({}, relayStyle, {'backgroundColor': '#ffaaaa'});
 
-        const relayStyle = {display: 'inline-block', margin: '10px', padding: '10px'};
-        const relayStyleOn = Object.assign({}, relayStyle, {'backgroundColor': 'red'});
+        const recipeStepStyle = {padding: '10px'};
+        const recipeStepActiveStyle = Object.assign({}, recipeStepStyle, {'backgroundColor': '#ffaaaa'});
 
+        const recipeButtonStyle = {margin: '10px'};
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
                 <div style={styles.container}>
 
-                    <Paper>
-                        <p>12.50°C Kühlschrank</p>
-                        <p>2.25°C Außen</p>
+                    <Paper className="panel">
+                        <div>12.50°C Kühlschrank</div>
+                        <div>2.25°C Außen</div>
                     </Paper>
 
-                    <Paper>
+                    <Paper className="panel">
+                        <TargetTemperatureSelector />
+                    </Paper>
+
+                    <Paper className="panel">
                         <Paper style={relayStyle}>Kühlung</Paper>
                         <Paper style={relayStyleOn}>Heizung</Paper>
                         <Paper style={relayStyle}>Kessel</Paper>
                     </Paper>
 
-                    <Paper>
-                        <TargetTemperatureSelector />
+                    <Paper className="panel">
+                        <Paper style={recipeStepStyle}>
+                            1. Heizen auf 64°C
+                        </Paper>
+                        <Paper style={recipeStepActiveStyle}>
+                            2. Halten für 15 Minuten
+                        </Paper>
+                        <Paper style={recipeStepStyle}>
+                            3. Heizen auf 72°C
+                        </Paper>
+                        <RaisedButton style={recipeButtonStyle} label="Starten" primary={true} />
+                        <RaisedButton style={recipeButtonStyle} label="Schritt Überspringen" />
+                        <RaisedButton style={recipeButtonStyle} label="Zurücksetzen" />
+                        <RaisedButton style={recipeButtonStyle} label="Rezept Bearbeiten" />
                     </Paper>
-
-                    <Dialog
-                        open={this.state.open}
-                        title="Super Secret Password"
-                        actions={standardActions}
-                        onRequestClose={this.handleRequestClose}
-                    >
-                        1-2-3-4-5
-                    </Dialog>
-                    <h1>material-ui</h1>
-                    <h2>example project</h2>
-                    <RaisedButton
-                        label="Super Secret Password"
-                        primary={true}
-                        onTouchTap={this.handleTouchTap}
-                    />
                 </div>
             </MuiThemeProvider>
         );
+
     }
 }
 
@@ -127,17 +119,18 @@ class TargetTemperatureSelector extends React.Component {
     render() {
         const standardActions = (
             <FlatButton
-                label="Okey"
+                label="OK"
                 secondary={true}
                 onTouchTap={this.handleRequestClose}
             />
         );
+        const buttonStyle = {
+            marginLeft: 10
+        };
         return (
             <div>
-                <Paper>
-                    <span>Zieltemperatur: 13°C</span>
-                    <RaisedButton label="Ändern" primary={true} onTouchTap={this.handleTouchTap} />
-                </Paper>
+                <span>Zieltemperatur: 13°C</span>
+                <RaisedButton label="Ändern" style={buttonStyle} secondary={true} onTouchTap={this.handleTouchTap} />
                 <Dialog open={this.state.open}
                     title="Zieltemperatur setzen"
                     actions={standardActions}
