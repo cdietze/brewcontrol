f2dfb51d44ffb72d08f9f5fab7b632e17206f7e6
Christoph Dietze <christoph.dietze@gmail.com>
2016-03-27 23:09:57 +0200

    Make maven run representation tests
    By default the Junit maven test runner won't run them.
    Instead of adding more configuration I converted the test class to top-level ones.

--- src/test/kotlin/brewcontrol/RepresentationTests.kt
+++ src/test/kotlin/brewcontrol/RepresentationTests.kt
@@ -5,51 +5,48 @@ import org.junit.Test
 
 val om = objectMapper
 
-class RepresentationTests {
+class StepsRepresentationTest {
+    private val heatStepJson = "{\"type\":\"Heat\",\"temperature\":50.0}"
 
-    class Steps {
-        private val heatStepJson = "{\"type\":\"Heat\",\"temperature\":50.0}"
-
-        @Test
-        fun serializesHeatStep() {
-            val heatStep = HeatStep(50.0)
-            assertThat(om.readTree(om.writeValueAsString(heatStep))).isEqualTo(om.readTree(heatStepJson));
-        }
-
-        @Test
-        fun deserializesHeatStep() {
-            assertThat(om.readValue(heatStepJson, HeatStep::class.java)).isEqualTo(HeatStep(50.0))
-        }
-
-        @Test
-        fun deserializesStepsPolymorphic() {
-            assertThat(om.readValue(heatStepJson, Step::class.java)).isEqualTo(HeatStep(50.0))
-        }
+    @Test
+    fun serializesHeatStep() {
+        val heatStep = HeatStep(50.0)
+        assertThat(om.readTree(om.writeValueAsString(heatStep))).isEqualTo(om.readTree(heatStepJson));
     }
 
-    class RecipeTest {
-        private val recipeJson = "{\"steps\":[{\"type\":\"Heat\",\"temperature\":50.0}]}"
-        @Test
-        fun serializes() {
-            val recipe = Recipe(listOf(HeatStep(50.0)))
-            assertThat(om.readTree(om.writeValueAsString(recipe))).isEqualTo(om.readTree(recipeJson));
-        }
-
-        @Test
-        fun defaultRecipeReadWrite() {
-            val recipe = Recipe()
-            val recipe2 = om.readValue(om.writeValueAsString(recipe), Recipe::class.java)
-            assertThat(recipe2).isEqualTo(recipe)
-        }
+    @Test
+    fun deserializesHeatStep() {
+        assertThat(om.readValue(heatStepJson, HeatStep::class.java)).isEqualTo(HeatStep(50.0))
     }
 
-    class RecipeProcessTest {
-        val recipeProcessJson = "{\"activeTaskIndex\":-1,\"tasks\":[{\"step\":{\"type\":\"Heat\",\"temperature\":10.0}}]}"
+    @Test
+    fun deserializesStepsPolymorphic() {
+        assertThat(om.readValue(heatStepJson, Step::class.java)).isEqualTo(HeatStep(50.0))
+    }
+}
 
-        @Test
-        fun serializes() {
-            val recipeProcess = RecipeProcess(Recipe(listOf(HeatStep(10.0))))
-            assertThat(om.readTree(om.writeValueAsString(recipeProcess))).isEqualTo(om.readTree(recipeProcessJson))
-        }
+class RecipeRepresentationTest {
+    private val recipeJson = "{\"steps\":[{\"type\":\"Heat\",\"temperature\":50.0}]}"
+    @Test
+    fun serializes() {
+        val recipe = Recipe(listOf(HeatStep(50.0)))
+        assertThat(om.readTree(om.writeValueAsString(recipe))).isEqualTo(om.readTree(recipeJson));
+    }
+
+    @Test
+    fun defaultRecipeReadWrite() {
+        val recipe = brewcontrol.Recipe()
+        val recipe2 = om.readValue(om.writeValueAsString(recipe), Recipe::class.java)
+        assertThat(recipe2).isEqualTo(recipe)
+    }
+}
+
+class RecipeProcessRepresentationTest {
+    val recipeProcessJson = "{\"activeTaskIndex\":-1,\"tasks\":[{\"step\":{\"type\":\"Heat\",\"temperature\":10.0}}]}"
+
+    @Test
+    fun serializes() {
+        val recipeProcess = RecipeProcess(Recipe(listOf(HeatStep(10.0))))
+        assertThat(om.readTree(om.writeValueAsString(recipeProcess))).isEqualTo(om.readTree(recipeProcessJson))
     }
 }
